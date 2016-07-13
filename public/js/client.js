c2617c3bf0c0a89641e9a5e8137471b0d52922e8
Ryan Mattson <ryansmattson@gmail.com>
2016-07-13 10:09:39 -0500

    Initial commit

---
+++ public/js/client.js
@@ -0,0 +1,180 @@
+angular.module('athleteApp', ['ngRoute']);
+
+angular.module('athleteApp').config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/badminton', {
+      templateUrl: 'views/badminton.html',
+      controller: 'BadmintonController'
+    })
+    .when('/judo', {
+      templateUrl: 'views/judo.html',
+      controller: 'JudoController'
+    })
+    .when('/synchronized-swimming', {
+      templateUrl: 'views/synchronized-swimming.html',
+      controller: 'SynchronizedSwimmingController'
+    })
+    .when('/table-tennis', {
+      templateUrl: 'views/table-tennis.html',
+      controller: 'TableTennisController'
+    })
+    .when('/triathlon', {
+      templateUrl: 'views/triathlon.html',
+      controller: 'TriathlonController'
+    })
+
+    $locationProvider.html5Mode(true);
+});
+
+
+
+
+angular.module('athleteApp').controller('BadmintonController', function($scope, $http){
+
+  $scope.randomAthlete;
+
+  function handleSuccess(response){
+      console.log('Success:', response.data);
+      $scope.randomAthlete = { name: response.data[0], origin: response.data[1], sport: response.data[2]};
+    }
+
+    function handleFailure(response){
+      console.log('Failure:', response.data);
+    }
+
+  function getAthlete(sport) {
+      $http({
+          method: 'GET',
+          url: '/olympics/' + sport
+      }).then(handleSuccess, handleFailure);
+    }
+
+  getAthlete('badminton');
+
+});
+
+angular.module('athleteApp').controller('JudoController', function($scope, $http){
+
+  $scope.randomAthlete;
+
+  function handleSuccess(response){
+      console.log('Success:', response.data);
+      $scope.randomAthlete = { name: response.data[0], origin: response.data[1], sport: response.data[2]};
+    }
+
+    function handleFailure(response){
+      console.log('Failure:', response.data);
+    }
+
+  function getAthlete(sport) {
+      $http({
+          method: 'GET',
+          url: '/olympics/' + sport
+      }).then(handleSuccess, handleFailure);
+    }
+
+  getAthlete('judo');
+});
+
+angular.module('athleteApp').controller('SynchronizedSwimmingController', function($scope, $http){
+
+  $scope.randomAthlete;
+
+  function handleSuccess(response){
+      console.log('Success:', response.data);
+      $scope.randomAthlete = { name: response.data[0], origin: response.data[1], sport: response.data[2]};
+    }
+
+    function handleFailure(response){
+      console.log('Failure:', response.data);
+    }
+
+  function getAthlete(sport) {
+      $http({
+          method: 'GET',
+          url: '/olympics/' + sport
+      }).then(handleSuccess, handleFailure);
+    }
+
+  getAthlete('synchronized-swimming');
+});
+
+angular.module('athleteApp').controller('TableTennisController', function($scope, $http){
+
+  $scope.randomAthlete;
+
+  function handleSuccess(response){
+      console.log('Success:', response.data);
+      $scope.randomAthlete = { name: response.data[0], origin: response.data[1], sport: response.data[2]};
+    }
+
+    function handleFailure(response){
+      console.log('Failure:', response.data);
+    }
+
+  function getAthlete(sport) {
+      $http({
+          method: 'GET',
+          url: '/olympics/' + sport
+      }).then(handleSuccess, handleFailure);
+    }
+
+  getAthlete('table-tennis');
+});
+
+angular.module('athleteApp').controller('TriathlonController', function($scope, $http){
+
+  $scope.randomAthlete;
+
+  function handleSuccess(response){
+      console.log('Success:', response.data);
+      $scope.randomAthlete = { name: response.data[0], origin: response.data[1], sport: response.data[2]};
+    }
+
+    function handleFailure(response){
+      console.log('Failure:', response.data);
+    }
+
+  function getAthlete(sport) {
+      $http({
+          method: 'GET',
+          url: '/olympics/' + sport
+      }).then(handleSuccess, handleFailure);
+    }
+
+  getAthlete('triathlon');
+});
+
+
+
+
+// app.controller('AthleteController', function($scope, $http){
+//
+//   $scope.randomAthleteArray = [];
+//
+//   function handleSuccess(response){
+//     console.log('Success:', response.data);
+//     $scope.randomAthleteArray.push({ name: response.data[0], origin: response.data[1], sport: response.data[2]});
+//   }
+//
+//   function handleFailure(response){
+//     console.log('Failure:', response.data);
+//   }
+//
+//   function getAthlete(sport) {
+//     $http({
+//         method: 'GET',
+//         url: '/olympics/' + sport
+//     }).then(handleSuccess, handleFailure);
+//   }
+//
+//   $scope.generateAthletesButton = function () {
+//     $scope.randomAthleteArray = [];
+//     getAthlete('badminton');
+//     getAthlete('judo');
+//     getAthlete('synchronized-swimming');
+//     getAthlete('table-tennis');
+//     getAthlete('triathlon');
+//   };
+//
+// })
